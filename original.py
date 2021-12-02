29b67152cc889374fecd5e0b1d1b1e8b3917cb20
kanta926 <93708396+kanta926@users.noreply.github.com>
2021-12-02 16:47:33 +0900

    A

--- original.py
+++ original.py
@@ -6,21 +6,6 @@ Created on Thu Nov 11 17:13:28 2021
 """
 
 import streamlit as st
-import time
-
-st.write("ようこそ。")
-st.write("あなたの水族館へ行きたい気持ちを読み取っています。")
-
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range (100):
-    latest_iteration.text(f"Lording...{i+1}")
-    bar.progress(i+1)
-    time.sleep(0.1)
-
-st.balloons()
-    
 
 st.title('あなたに推したい水族館')
 
